1305d2fcc517585f0a01ad52ab795b2bfa3aab45
Akshat Sipany <akshatsipany2106@gmail.com>
2026-06-14 15:17:20 +0530

    backend init

---
+++ backend/src/test/java/com/assignment/typeahead/TypeaheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.typeahead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeaheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
